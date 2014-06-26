cd7c3456473e05601d97a0ac87b8cc2f399a47d8
nelsonic <nodecoder@gmail.com>
2014-06-26 22:38:55 +0100

    still passes "Hello Hapi" - makemehapi tutorial

--- examples/hellohapi.js
+++ examples/hellohapi.js
@@ -1,17 +1,17 @@
 // Start the app from your command line with: node hellohapi.js
-// then visit: http://localhost:3000/YOURNAME in your browser
+// then visit: http://localhost:8080 in your browser
 
 var Hapi = require('hapi');
-var server = new Hapi.Server('0.0.0.0', 3000);
+var server = new Hapi.Server('0.0.0.0', 8080);
 
 server.route({ 
 	method: 'GET',
-	path: '/{yourname*}',
+	path: '/',
 	handler: function(req, reply) {
-		reply('Hello ' + req.params.yourname + '!')
+		reply('Hello Hapi')
 	}
 });
 
 server.start(function(){ // boots your server
-	console.log('Now Visit: http://localhost:3000/YOURNAME')
+	console.log('Now Visit: http://localhost:8080')
 });
