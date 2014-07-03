1b1c69484d64cd1f82aba685358785cba8debafa
nelsonic <nodecoder@gmail.com>
2014-07-03 15:41:24 +0100

    passes makemehapi DIRECTORIES

---
+++ makemehapi/04-DIRECTORIES.js
@@ -0,0 +1,12 @@
+var Hapi = require('hapi');
+var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
+
+server.route({
+  path: "/foo/bar/baz/{param}",
+  method: 'GET',
+  handler: {
+    directory: { path: './public' }
+  }
+});
+
+server.start();
