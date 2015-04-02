729c049edd0f46644468baeee6cdd84420a2c357
nelsonic <contact.nelsonic@gmail.com>
2015-04-02 17:28:45 +0100

    updates static file server example

--- examples/staticfiles.js
+++ examples/staticfiles.js
@@ -1,20 +1,37 @@
-var path = require('path');
-var pub = path.normalize(__dirname + '/public');
+var Path = require('path');
 var Hapi = require('hapi');
 var server  = new Hapi.Server();
 var port    = process.env.PORT || 5000;
 
 server.connection({ port: port });
 
-server.route({
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    path: Path.join(__dirname, 'views')
+});
+
+server.route([  
+  { path: '/',
+    method: 'GET',
+    config: {
+      auth: false,
+      handler: function(request, reply) {
+        reply.view("index");
+      }
+    }
+  },
+  {
     method: 'GET',
     path: '/public/{param*}',
     handler: {
         directory: {
-            path: pub
+            path: Path.normalize(__dirname + '/public')
         }
     }
-});
+  }
+]);
 
 server.start(function(){
   console.log('Static Server Listening on : http://127.0.0.1:' +port);
