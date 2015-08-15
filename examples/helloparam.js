e4ad2cc85da23a9bd3ccf0721f548b1083661347
Nelson <contact.nelsonic@gmail.com>
2015-08-15 20:51:26 +0100

    PR pending response to issue: https://github.com/hapijs/hapi/issues/2715

--- examples/helloparam.js
+++ examples/helloparam.js
@@ -3,30 +3,22 @@
    node helloparam.js
    then visit: http://localhost:3000/YOURNAME
 */
-var Hapi = require('hapi');
-
+var Hapi   = require('hapi');
 var server = new Hapi.Server();
+
 server.connection({
     host: '0.0.0.0',
     port: 3000
 });
 
-
 server.route({
 	method: 'GET',
 	path: '/{p*}',
 	handler: function(req, reply){
-		reply('Hello ' + req.params.p)
-		server.log(["test"], "This is my log entry!");
+		return reply('Hello ' + req.params.p)
 	}
 })
 
 server.start(function(){
   console.log('Now Visit: http://localhost:3000/YOURNAME')
-});
-
-server.on("log", function(event, tags) {
-    var tagsJoined = Object.keys(tags).join();
-    var message = event.data;
-    console.log("Log entry [" + tagsJoined + "] (" + (message || "") + ")");
 });
