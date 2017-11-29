a141c0dbea880404dff666226c7750c035009e37
Danielwhyte <danwhy@gmail.com>
2017-11-29 13:45:34 +0000

    updates tests to work with hapi 17 #94

--- test/hellovalidate.test.js
+++ test/hellovalidate.test.js
@@ -1,33 +1,29 @@
-var Code = require('code');   // assertion library
-var Lab = require('lab');
-var lab = exports.lab = Lab.script();
+const Code = require('code');   // assertion library
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
 
-var server = require("../examples/hellovalidate"); // require ../index.js
+const server = require("../examples/hellovalidate"); // require ../index.js
 
 lab.experiment("Joi Validation Test", function() {
   // tests
-  lab.test("GET /j should fail due to insufficient name length", function(done) {
-    var options = {
+  lab.test("GET /j should fail due to insufficient name length", async function() {
+    const options = {
       method: "GET",
       url: "/j"
     };
-    server.inject(options, function(response) {
-      Code.expect(response.statusCode).to.equal(400);  //  Expect http response status code to be 400 ("Bad Request")
-      server.stop();
-      done();                                         // done() callback is required to end the test.
-    });
+    const response = await server.inject(options);
+    Code.expect(response.statusCode).to.equal(400);  //  Expect http response status code to be 400 ("Bad Request")
+    await server.stop();
 	});
 
-  lab.test("GET /jo should pass", function(done) {
-    var options = {
+  lab.test("GET /jo should pass", async function() {
+    const options = {
       method: "GET",
       url: "/jo"
     };
-    server.inject(options, function(response) {
-      Code.expect(response.statusCode).to.equal(200);  //  Expect http response status code to be 200 ("Ok")
-      Code.expect(response.result).to.have.length(7); // Expect result to be "Hai jo!" (7 chars long)
-      server.stop();
-      done();                                         // done() callback is required to end the test.
-    });
+    const response = await server.inject(options);
+    Code.expect(response.statusCode).to.equal(200);  //  Expect http response status code to be 200 ("Ok")
+    Code.expect(response.result).to.have.length(7); // Expect result to be "Hai jo!" (7 chars long)
+    await server.stop();
   });
 });
